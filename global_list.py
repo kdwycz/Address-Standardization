582bd39ad64ec913d64bbd3a0c8bd3a2a58dbc36
kdwycz <kdwycz@gmail.com>
2015-01-20 21:33:06 +0800

    2.0
    使用openpyxl模块，大幅简化代码。
    优化写法，加入异常判断

--- global_list.py
+++ global_list.py
@@ -6,12 +6,12 @@ __author__ = 'kdwycz'
 # APIKEY 请自行申请并替换下列字符串，可新增
 # http://developer.baidu.com/map/index.php
 KEY = (
-	'1A4RCEBGsTTKngDpX6G62sca',
-	'8UcQSlGy6FBQXZEfeV1QTNc6',
-	'3fvsqvuoWOBIIAzUyw3KS6fK',
-	'7wy6KxfMNhxtluBGGWVRlu8p',
-	'VUrGoUHK9G7uttdfEkUQIgZK',
-	'x65QkAbw3sh6EEbKHaanaetb',
+	u'1A4RCEBGsTTKngDpX6G62sca',
+	u'8UcQSlGy6FBQXZEfeV1QTNc6',
+	u'3fvsqvuoWOBIIAzUyw3KS6fK',
+	u'7wy6KxfMNhxtluBGGWVRlu8p',
+	u'VUrGoUHK9G7uttdfEkUQIgZK',
+	u'x65QkAbw3sh6EEbKHaanaetb',
 )
 
 APINUM = len(KEY)
@@ -19,13 +19,13 @@ APINUM = len(KEY)
 NUM = 0
 
 # 前三级行政区的标志
-DIVISIONS = ('省','市','区','盟','旗','县')
+DIVISIONS = (u'省',u'市',u'区',u'盟',u'旗',u'县')
 
+ROOTNAME = u'主地址'
+HOTELNAME = u'旅馆代码'
 
-#跳过对某一步查询的处理 True or False
-STEP = {
-	'IB' : True,
-	'LG' : True,
-	'LH' : True,
-	'YH' : True,
-}
+ROOTSET = ['IB','YH','LH']
+MIXSET = 'LG'
+
+#Linux为0;windows为1.程序会自行判断.影响表格行列起始值
+OS = 0
